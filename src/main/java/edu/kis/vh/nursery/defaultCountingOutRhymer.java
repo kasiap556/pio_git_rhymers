3c50b8f123ab9791f2e2e66a977380a859165e98
kasia <katarzynapietrzyk2806>
2023-04-14 11:49:51 +0200

    5. zmiana nazw zmiennych

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,33 +2,33 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-    private int[] NUMBERS = new int[12];
+    private int[] numbers = new int[12];
 
-    public int total = -1;
+    public int top = -1;
 
     public void countIn(int in) {
         if (!isFull())
-            NUMBERS[++total] = in;
+            numbers[++top] = in;
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return top == -1;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return top == 11;
     }
 
     protected int peekaboo() {
         if (callCheck())
             return -1;
-        return NUMBERS[total];
+        return numbers[top];
     }
 
     public int countOut() {
         if (callCheck())
             return -1;
-        return NUMBERS[total--];
+        return numbers[top--];
     }
 
 }
